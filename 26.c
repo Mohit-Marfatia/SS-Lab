0d181aca7a2266526763adfdbb86e2e00c7be9c9
Mohit-Marfatia <marfatia.projects@gmail.com>
2024-08-31 06:40:21 +0530

    question 26

---
+++ 26.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    execl("./a.out", "./a.out", NULL);
+    printf("Some error occured");
+    return 0;
+}
+
+/*
+Output:
+(base) mohit-marfatia@mohit-marfatia:~/College/Software-Systems/exercise$ cc -o b.out 26.c
+(base) mohit-marfatia@mohit-marfatia:~/College/Software-Systems/exercise$ ./b.out
+Child 1 PID: 6586 is running...
+Parent PID: 6585 is waiting for Child 2 PID: 6587 to complete.
+Child 2 PID: 6587 is running...
+Child 3 PID: 6588 is running...
+Child 1 PID: 6586 is exiting.
+Child 2 PID: 6587 is exiting.
+Parent PID: 6585 detected that Child 2 PID: 6587 has finished.
+Parent PID: 6585 detected that Child PID: 6586 has finished.
+Child 3 PID: 6588 is exiting.
+Parent PID: 6585 detected that Child PID: 6588 has finished.
+Parent PID: 6585 is exiting.
+
+*/
